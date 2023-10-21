6e2eb32e9f6ad308207e895d72e8d3413f90a717
Aman <akoua@umich.edu>
2023-10-21 19:24:21 -0400

    Don't render profile icon if on login or signup page

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import logo from "../assets/MoneyMastersLogo.jpg";
 
 const Navbar = () => {
+  const location = useLocation();
+  const pagesWithoutProfileImg = ["/login", "/signup"];
+  const isPageWithoutProfileImg = pagesWithoutProfileImg.includes(
+    location.pathname
+  );
+
   return (
     <div className="flex flex-row">
       <div className="h-14 w-full flex flex-row">
@@ -10,7 +17,9 @@ const Navbar = () => {
           MoneyMasters
         </div>
       </div>
-      <div className="bg-gray-300 w-10 h-10 rounded-full mr-5 mt-auto mb-auto"></div>
+      {!isPageWithoutProfileImg && (
+        <div className="bg-gray-300 w-10 h-10 rounded-full mr-5 mt-auto mb-auto"></div>
+      )}
     </div>
   );
 };
