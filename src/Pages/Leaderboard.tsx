e34c2fbb820c956ca1af480ac78d5a9d8bafc397
Aman <akoua@umich.edu>
2023-10-22 06:34:14 -0400

    Prevent access to leaderboard page if not logged in

--- src/Pages/Leaderboard.tsx
+++ src/Pages/Leaderboard.tsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
 import Drawer from "../Components/Drawer";
 
 import { ProfileContext } from "../Context/ProfileContext";
 
 const Leaderboard = () => {
   const userProfile = useContext(ProfileContext);
-  console.log(userProfile);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+    if (!user) {
+      navigate("/login");
+    }
+  }, []);
 
   return (
     <div>
