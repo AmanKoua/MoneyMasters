7a7bd32889ce707260cd6803f7180a9ab8919724
Aman <akoua@umich.edu>
2023-10-22 06:39:08 -0400

    Prevent access to budgeting page when not logged in

--- src/Pages/BudgetingPage.tsx
+++ src/Pages/BudgetingPage.tsx
@@ -1,23 +1,33 @@
-import React from 'react'
-import Drawer from '../Components/Drawer'
+import React, { useEffect } from "react";
+import Drawer from "../Components/Drawer";
+import { useNavigate } from "react-router-dom";
+
 import { Outlet } from "react-router-dom";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const BudgetingPage = () => {
-    return (
-        <>
-            <div className="overflow-y-auto h-screen">
-                <Drawer></Drawer>
-                <h3 className="w-3/4  ml-auto text-center text-6xl">Budgeting</h3>
-                <div className=" pt-10 w-3/4 h-full ml-auto">
-                    <div className=" w-3/4 ml-auto mr-auto mt-auto flex flex-col">
-                        <Outlet />
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-}
+  const navigate = useNavigate();
 
-export default BudgetingPage
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+    if (!user) {
+      navigate("/login");
+    }
+  }, []);
 
+  return (
+    <>
+      <div className="overflow-y-auto h-screen">
+        <Drawer></Drawer>
+        <h3 className="w-3/4  ml-auto text-center text-6xl">Budgeting</h3>
+        <div className=" pt-10 w-3/4 h-full ml-auto">
+          <div className=" w-3/4 ml-auto mr-auto mt-auto flex flex-col">
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BudgetingPage;
