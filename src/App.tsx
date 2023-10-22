bf821a9517ed101ceeeaaf3c63983f13de773bd4
Ibrahim-gw8885 <gw8885@wayne.edu>
2023-10-22 09:00:27 -0400

    merge conflict app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -33,22 +33,10 @@ function App() {
               <Route path="/login" element={<Login></Login>} />
               <Route path="/signup" element={<Signup />} />
               <Route path="/" element={<Home />} />
-<<<<<<< HEAD
-              <Route path="/aboutus" element={<AboutUs />} />
-              <Route path="/leaderboard" element={<Leaderboard />} />
-              <Route path="/modules" element={<ModulePage />} />
-              <Route path="/modules/budgeting" element={<BudgetingPage/>}>
-                <Route index element = {<Navigate replace to="ex1"/>}/>
-                <Route path="ex1" element={<B_1/>} />
-                <Route path="ex2" element={<B_2/>} />
-              </Route>
-
-=======
               <Route path="/modules" element={<ModulePage />} />
               <Route path="/leaderboard" element={<Leaderboard />} />
               <Route path="/aboutus" element={<AboutUs />} />
               <Route path="/portfolio" element={<Portfolio />} />
->>>>>>> f48c18372524ac284af1486859c995d986206e46
               <Route path="*" element={<ErrorPage />} />
             </Routes>
           </div>
